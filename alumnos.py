1f7fc0b41319732d48016e8951104426ec07af7f
FelipeDili <dilisciafelipe@gmail.com>
2024-10-13 18:43:51 -0300

    veamos si funciona

--- alumnos.py
+++ alumnos.py
@@ -24,18 +24,27 @@ def generarAlumnos(cantidad):
     dni_generado = random.randint(30000000, 45000000)
     
     while dni_generado in [alumno["DNI"] for alumno in alumnos]:
-      dni_generado = random.randint(30000000, 45000000)
+      dni_generado = random.randint(30000, 45000000)
       
     legajo_generado = random.randint(800000, 1200000)
     
     while legajo_generado in [alumno["LU"] for alumno in alumnos]:
       legajo_generado = random.randint(800000, 1200000)
     
+    mail_generado = str(f"{nombre[0].lower()}{apellido.lower()}.{legajo_generado}@edau.edu.ar")
+
+    while mail_generado in [alumno["Mail"] for alumno in alumnos]:
+      mail_generado = str(f"{nombre[0].lower()}{apellido.lower()}.{legajo_generado}@edau.edu.ar")
+
+    estado = 'Activo'
+    
     alumno = {
       "nombre": nombre,
       "apellido": apellido,
       "DNI": dni_generado,
-      "LU": legajo_generado
+      "LU": legajo_generado,
+      "Mail": mail_generado,
+      "Estado": estado
     }
     alumnos.append(alumno)
 
@@ -49,23 +58,47 @@ def listarAlumnos(alumnos):
     print(f"Apellido: {alumno["apellido"]}")
     print(f"D.N.I: {alumno["DNI"]:,}")
     print(f"L.U: {alumno["LU"]:,}")
+    print(f"Mail: {alumno["Mail"]}")
     print("_________________________")
     print("")
 
-  print(f"Alumnos totales: {len(alumnos)}")
+    print(f"Alumnos activos totales: {sum(1 for alumno in alumnos if alumno['Estado'] == 'Activo')}")
+  return
+
+def listarAlumnosInactivos(alumnos):
+  for alumno in alumnos:
+    if alumno['Estado'] == 'Inactivo':
+      print(f"Nombre: {alumno["nombre"]}")
+      print(f"Apellido: {alumno["apellido"]}")
+      print(f"D.N.I: {alumno["DNI"]:,}")
+      print(f"L.U: {alumno["LU"]:,}")
+      print(f"Mail: {alumno["Mail"]}")
+      print("_")
+      print("")
+  print(f"Alumnos inactivos totales: {sum(1 for alumno in alumnos if alumno['Estado'] == 'Inactivo')}")
   return
 
 def nuevoAlumno(nombre, apellido, dni, alumnos):
+  legajoU = random.randint(800000, 1200000)
   alumno = {
     "nombre": nombre,
     "apellido": apellido,
     "DNI": dni,
-    "LU": random.randint(800000, 1200000)
+    "LU": legajoU,
+    "Mail": generarMail(nombre, apellido, legajoU),
+    "Estado": 'Activo'
   }
   
   alumnos.append(alumno)
   
   return alumnos
+
+def generarMail(nombre, apellido, legajo):
+  '''
+  Genera un mail teniendo en cuenta la primera letra del nombre, el apellido y el legajo
+  '''
+  mail = str(f"{nombre[0].lower()}{apellido.lower()}.{legajo}@edau.edu.ar")
+  return mail
 
 def modificarAlumnoPorLU(LU, propiedad, nuevoValor, alumnos):
   for alumno in alumnos:
@@ -90,6 +123,9 @@ def encontrarPorLegajo():
   return [encontrado, alumnoEncontrado]
 
 def encontrarPorDni():
+  '''
+  Busca alumnos por DNI
+  '''
   dni = int(input("Ingrese el dni del alumno: "))
   
   for alumno in alumnos:
@@ -98,12 +134,45 @@ def encontrarPorDni():
       print(f"Apellido: {alumno["apellido"]}")
       print(f"D.N.I: {alumno["DNI"]:,}")
       print(f"L.U: {alumno["LU"]:,}")
+      print(f"Mail: {alumno["Mail"]:,}")
       print("_________________________")
       print("")
       break
   else:
-    print("No se encontró un alumno con el legajo ingresado.")
+    print("No se encontró un alumno con el DNI ingresado.")
   return
+
+def borrarAlumnoLogico(LU, alumnos):
+    for alumno in alumnos:
+        if alumno["LU"] == LU:
+            alumno["Estado"] = "Inactivo"
+            print(f"El alumno con LU {LU} ha sido marcado como Inactivo.")
+            return alumnos
+    print(f"No se encontró un alumno con el LU {LU}.")
+    return alumnos
+
+def pideDNI(alumnos):
+    '''
+    Controla que el DNI ingresado no haya sido utilizado por otro alumno
+    '''
+    while True:
+      dni = int(input("Ingrese el DNI del alumno: "))
+      if dni <= 0:
+          print("El DNI debe ser un número positivo.")
+          continue
+      if len(str(dni)) < 7 or len(str(dni)) > 8:
+          print("El DNI debe tener entre 7 y 8 dígitos.")
+          continue
+
+      encontrado = False 
+      for alumno in alumnos:
+          if alumno["DNI"] == dni:
+              encontrado = True
+              break
+      if encontrado:
+          print("DNI existente. Por favor, ingrese otro.")
+      else:
+          return dni 
 
 if __name__ == "__main__":
   print(len(alumnos))
--- clases_materias.py
+++ clases_materias.py
@@ -1,8 +1,10 @@
 import random
+import datetime
 
 # Materias & Clases
 
-materias = [{"id": 1, "nombre": "Matemática Discreta"},
+materias = [
+  {"id": 1, "nombre": "Matemática Discreta"},
   {"id": 2, "nombre": "Álgebra y Geometría Analítica"},
   {"id": 3, "nombre": "Análisis Matemático I"},
   {"id": 4, "nombre": "Algoritmos y Estructuras de Datos"},
@@ -19,17 +21,107 @@ def generarClases(materias, cantidad):
   Genera una lista de clases basado en las materias que tenemos al inicio del modulo de manera aleatoria para inicializar el programa con datos en memoria.
   '''
   clases = []
-  for _ in range(cantidad):
+  for i in range(cantidad):
     clase = {
+      "id": i+1000,
       "dia": random.randint(0, 4),
       "turno": random.randint(0, 2),
-      "anio": 2024,
+      "anio": datetime.datetime.now().year,
       "cuatrimestre": random.randint(0, 1),
-      "materiaId": random.choice(materias)["id"]
+      "materiaId": random.choice(materias)["id"],
+      "Estado": "Activa"
     }
     clases.append(clase)
   return clases
 
+def crearClase(clases, materias):
+    if len(clases) > 0:
+        nuevoId = clases[-1]["id"] + 1 
+    else:
+        nuevoId = 1000 
+
+    while True:
+        dia = int(input("Ingrese el dia de la clase (0: Lunes, 1: Martes, 2: Miercoles, 3: Jueves, 4: Viernes): "))
+        if 0 <= dia <= 4:
+            break
+        else:
+            print("Opcion invalida, por favor ingrese una opcion correcta (0: Lunes, 1: Martes, 2: Miercoles, 3: Jueves, 4: Viernes)")
+    
+    while True:
+        turno = int(input("Ingrese el turno de la clase (0: Mañana, 1: Tarde, 2: Noche): "))
+        if 0 <= turno <= 2:
+            break
+        else:
+            print("Opcion invalida, por favor ingrese una opcion correcta (0: Mañana, 1: Tarde, 2: Noche)")
+    
+    while True:
+        print("Listado materias: ")
+        for materia in materias:
+            print(f"{materia['id']}: {materia['nombre']}")
+        id = int(input("Ingrese el ID de la materia de la clase: "))
+        if 1 <= id <= 10:
+            break
+        else:
+            print("ID invalido, por favor ingrese un ID correcto (entre 1 y 10)")
+
+    CualCuatrimestre = datetime.datetime.now().month
+    cuatrimestreActual = 0 if CualCuatrimestre < 7 else 1
+
+    claseNueva = {
+       "id": nuevoId,
+       "dia": dia, 
+       "turno": turno,
+       "anio": datetime.datetime.now().year,
+       "cuatrimestre": cuatrimestreActual,
+       "materia_id": id,
+       "Estado": "Activa"
+       }
+    
+    clases.append(claseNueva)
+    print("Clase creada con éxito:", claseNueva)
+
+def buscarClasePorId(clases, id):
+    for clase in clases:
+        if clase["id"] == id:
+            return clase
+    return None
+
+def modificarClase(clases):
+    id = int(input("Ingrese el ID de la clase que desea modificar: "))
+    claseEncontrada = buscarClasePorId(clases, id)
+    if claseEncontrada:
+      print(f"Clase encontrada: {claseEncontrada}") 
+      while True:
+          nuevoDia = int(input("Ingrese el nuevo día de la clase (0: Lunes, 1: Martes, 2: Miércoles, 3: Jueves, 4: Viernes): "))
+          if 0 <= nuevoDia <= 4:
+              claseEncontrada["dia"] = nuevoDia
+              break
+          else:
+              print("Opción inválida. Ingrese un día válido (0: Lunes, 1: Martes, 2: Miércoles, 3: Jueves, 4: Viernes).")
+      while True:
+          nuevoTurno = int(input("Ingrese el nuevo turno de la clase (0: Mañana, 1: Tarde, 2: Noche): "))
+          if 0 <= nuevoTurno <= 2:
+              claseEncontrada["turno"] = nuevoTurno
+              break
+          else:
+              print("Opción inválida. Ingrese un turno válido (0: Mañana, 1: Tarde, 2: Noche).")
+      print(f"Clase actualizada: {claseEncontrada}")
+    else:
+      print("No se encontró una clase con el ID ingresado.")
+
+def eliminarClase(clases):
+    id = int(input("Ingrese el ID de la clase que desea eliminar: "))
+    for clase in clases:
+        if clase["id"] == id:
+            if clase["Estado"] == "Activa":
+                clase["Estado"] = "Inactiva"
+                print(f"Clase eliminada: {clase}")
+            else:
+                print("La clase ya está eliminada.")
+                print(f"{clase}")
+            return
+    print("No se encontró una clase con el ID ingresado.")
+  
 def asignarNuevaClase(LU, clase, alumnos):
   for alumno in alumnos:
     if alumno["LU"] == LU:
