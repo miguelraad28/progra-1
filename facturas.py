5cd935fbfec3dbd6ca9e2197c720c9e2bd00b5d6
miguelraad28 <miguelraad2020@gmail.com>
2024-11-23 13:54:37 -0300

    .

--- facturas.py
+++ facturas.py
@@ -1,5 +1,7 @@
 import json
 
+costo_por_clase = 50000
+
 def abrirArchivoFacturas():
   '''
   Abre el archivo json de las facturas
@@ -43,3 +45,42 @@ def reescribirArchivoFacturas(facturas):
       pass 
 
   return success
+
+
+def marcarComoPagada(factura):
+  '''
+  Marca una factura como pagada.
+  Args:
+    factura: dict - Diccionario con los datos de la factura.
+  Returns:
+    bool - True si se pudo marcar, False si no.
+  '''
+  success, facturas = abrirArchivoFacturas()
+  if not success:
+    return False
+
+  for f in facturas:
+    if f["id"] == factura["id"]:
+      f["pagada"] = True
+      break
+
+  return reescribirArchivoFacturas(facturas)
+
+def listarMorosos(facturas, alumnos):
+  '''
+  Lista los alumnos morosos.
+  Args:
+    facturas: list - Lista de facturas.
+    alumnos: list - Lista de alumnos.
+  Returns:
+    None
+  '''
+  morosos = []
+  for alumno in alumnos:
+    deuda = 0
+    for factura in facturas:
+      if factura["alumnoLU"] == alumno["LU"] and not factura["pagada"]:
+        deuda += len(factura.clases) * costo_por_clase
+    if deuda > 0:
+      morosos.append({"LU": alumno["LU"], "nombre": alumno["nombre"],"apellido": alumno["apellido"], "deuda": deuda})
+  return morosos
